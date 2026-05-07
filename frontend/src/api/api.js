1a106b7b690925f1dc63d2fb0f512c85897a1fa8
lakshay-bhargav3761 <lakshaybhargav45@gmail.com>
2026-05-08 01:21:12 +0530

    Connected frontend to backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://expense-tracker-1qg4.onrender.com/api",
 });
 
 /* =========================
